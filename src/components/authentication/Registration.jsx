d87421e305fb620a768d8186d465dfa84baa0fc2
koobcbc <kook@wra.net>
2020-08-15 06:01:58 +0900

    sign-up form done

--- src/components/authentication/Registration.jsx
+++ src/components/authentication/Registration.jsx
@@ -5,66 +5,96 @@ import apiUrl from '../ApiConfig'
 import axios from 'axios'
 
 
-const Registration = ({ handleChange, handleSubmit }) => {
+const Registration = ({ handleSubmitFromApp }) => {
   
+    const [passwordMatch, setPasswordMatch] = useState(true)
+
     const [userInfo, setUserInfo] = useState({
         username: "",
         password: "",
         password_confirmation: ""
     })
 
-    const handleChange = (e) => {
-        console.log('handling change in ByCountry input state', e.target.value)
-        const userInput = e.target.value
-        setUserInfo(userInput)
+    const handleChange = (event) => {
+        //console.log("event", event.target.name, event.target.value);
+        setUserInfo({
+          ...userInfo,
+          [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        document.getElementById("registration_form").reset();
+        console.log('handling Submit' - userInfo) 
+        e.preventDefault()
+        if (userInfo.password === userInfo.password_confirmation){
+            handleSubmitFromApp(userInfo)
+            setUserInfo({})
+        }
+        else {
+            console.log('password matching', passwordMatch)
+            setPasswordMatch(false)
+        }
     }
     
+    console.log(userInfo)
+    console.log(passwordMatch)
+
     return (
         <>
-            <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formBasicUsername">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                type="username"
-                placeholder="Username"
-                value={userInfo.username}
-                name="username"
-                onChange={handleChange}
-                required
-                />
-            </Form.Group>
+            <Form onSubmit={handleSubmit} id="registration_form">
+                <Form.Group controlId="formBasicUsername">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                    type="username"
+                    placeholder="Username"
+                    value={userInfo.username}
+                    name="username"
+                    onChange={handleChange}
+                    required
+                    />
+                </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                type="password"
-                placeholder="Password"
-                value={userInfo.password}
-                name="password"
-                onChange={handleChange}
-                required
-                />
-            </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    value={userInfo.password}
+                    name="password"
+                    onChange={handleChange}
+                    required
+                    />
+                </Form.Group>
 
-            <Form.Group>
-                <Form.Label>
-                Password Confirmation
-                </Form.Label>
-                <Form.Control
-                type="password"
-                placeholder="Password Confirmation"
-                type="password"
-                value={userInfo.password_confirmation}
-                name="password_confirmation"
-                onChange={handleChange}
-                required
-                />
-            </Form.Group>
+                <Form.Group>
+                    <Form.Label>
+                    Password Confirmation
+                    </Form.Label>
+                    <Form.Control
+                    type="password"
+                    placeholder="Password Confirmation"
+                    type="password"
+                    value={userInfo.password_confirmation}
+                    name="password_confirmation"
+                    onChange={handleChange}
+                    required
+                    />
+                </Form.Group>
 
-            <Button variant="primary" type="submit">
-                Register
-            </Button>
+                <Button variant="primary" type="submit">
+                    Register
+                </Button>
             </Form>
+
+            {passwordMatch ? null :
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                <strong>Error! </strong> Password Not Matching.
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div> 
+            }
     </>
   );
 };
--- src/components/routes/Home/Home.jsx
+++ src/components/routes/Home/Home.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import logo from '../../bookworm_logo.png'
+import logo from '../../../bookworm_logo.png'
 import { Link } from 'react-router-dom'
 
 import Login from './Login'
 
 import Button from 'react-bootstrap/Button';
 
-const Home = () => {
+const Home = ( ) => {
     return(
         <>
             <h1>BOOKWORM</h1>
-            <a><img src={logo} width="200px"/></a>
-            {/* <Login /> */}
+            <img src={logo} width="200px"/>
+            <Login />
             <Link to="/signup"><Button>Sign Up</Button></Link>
         </>
     )
