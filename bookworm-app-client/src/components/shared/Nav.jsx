1713039c931df65e99dff68d057e76bab6b0bf99
koobcbc <kook@wra.net>
2020-08-16 19:25:30 +0900

    created Nav component

---
+++ bookworm-app-client/src/components/shared/Nav.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom'
+
+const Nav = () => {
+
+    return(
+        <>
+            <div className="small-screen">
+                <nav>
+                    <NavLink to='/search'>Search a Book</NavLink>
+                    <NavLink to='/mypage'>My Page</NavLink>
+                    <NavLink to='/about'>About</NavLink>
+                    <NavLink to='/'>Log Out</NavLink>
+                </nav>
+            </div>
+            <div className="small-screen">
+                <NavLink to='/mypage'>My Page</NavLink>
+                <NavLink to='/'>Log Out</NavLink>
+                <nav>
+                    <NavLink to='/search'>Search a Book</NavLink>
+                    <NavLink to='/about'>About</NavLink>
+                </nav>
+            </div>
+            
+        </>
+    )
+}
+
+export default Nav
